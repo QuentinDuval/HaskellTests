9effde17837c34581576266aec90efa7229bcbd9
QuentinDuval <senmenty@gmail.com>
2015-05-06 21:49:59 +0200

    Polyvariadic functions

--- src/PolyvariadicFunction.hs
+++ src/PolyvariadicFunction.hs
@@ -1,5 +1,38 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances, OverloadedStrings #-}
+
 module PolyvariadicFunction where
 
+import Data.Text
 
--- TODO - Make some kind of printf
--- TODO - Try to use rankNTypes
+
+-- Improving the solution from:
+-- From http://stackoverflow.com/questions/3467279/how-to-create-a-polyvariadic-haskell-function
+
+class SumRes a r where 
+    sumOf :: a  -> r
+
+
+-- For integers
+
+instance SumRes Integer Integer where
+    sumOf = id
+
+instance (Integral a, SumRes Integer r) => SumRes Integer (a -> r) where
+    sumOf x = sumOf . (x +) . toInteger
+
+
+-- For strings
+
+instance SumRes Text Text where
+    sumOf = id
+
+instance (SumRes Text r) => SumRes Text (Text -> r) where
+    sumOf x = sumOf . append  x
+
+
+test1 :: Integer
+test1 = sumOf (1 :: Integer) 2 3 4 5
+
+test2 :: Text
+test2 = sumOf ("toto" :: Text) ("titi" :: Text) ("tutu" :: Text)
+
