eb1c9d0d606a2271b2af7cf9967555932779a1f9
QuentinDuval <senmenty@gmail.com>
2015-05-14 12:02:16 +0200

    Conduit

--- src/ConduitTry.hs
+++ src/ConduitTry.hs
@@ -2,18 +2,19 @@
 module ConduitTry where
 
 import Control.Monad
-import Control.Monad.IO.Class
-import Data.ByteString as B
+import Control.Monad.Trans.Class(lift)
+import Control.Monad.Trans.State(get, put)
 import Data.Conduit as C
-import Data.Conduit.Binary
-import Data.Conduit.List as C
-import Data.Monoid
+import Data.Conduit.Binary(sourceHandle, sinkHandle)
+import Data.Conduit.List as C (map)
+import Data.Conduit.Lift(evalStateLC)
+import Data.Monoid((<>))
 import Data.Text as T
-import qualified Data.Text.Lazy as T
+import Data.Text.Lazy(toStrict)
 import Data.Text.Lazy.Builder as TB
-import Data.Text.Lazy.Builder.Int as TB
-import Data.Text.Encoding as E
-import System.IO as S
+import Data.Text.Lazy.Builder.Int(decimal)
+import Data.Text.Encoding(decodeUtf8, encodeUtf8)
+import System.IO
 import System.Random
 
 
@@ -21,29 +22,26 @@ import System.Random
 -- TODO - Investigate the possibility of parallelism (each stage in a spark)
 
 addRandNumber :: (Monad m, RandomGen g) => g -> Conduit Text m (Text, Int)
-addRandNumber = loop where
-   loop g = do
-      v <- await
-      case v of
-         Nothing -> return ()
-         Just t  -> do
-            let (i, g') = next g
-            yield (t, i)
-            loop g'
+addRandNumber g = void $ evalStateLC g $
+   awaitForever $ \t -> do
+      g' <- lift get
+      let (i, g'') = next g'
+      yield (t, i)
+      lift $ put g''
 
 
 concatBoth :: (Monad m) => Conduit (Text, Int) m Text
 concatBoth = awaitForever $ \(t, n) -> do
    let t' = TB.fromText t <> " " <> decimal n <> "\n"
-   yield $ T.toStrict $ TB.toLazyText t'
+   yield $ toStrict $ TB.toLazyText t'
 
 
 testConduit :: Handle -> Handle -> IO ()
 testConduit input output = do
    g <- getStdGen 
-   runConduit $ sourceHandle input $$ C.map E.decodeUtf8
+   runConduit $ sourceHandle input $$ C.map decodeUtf8
       $= addRandNumber g $= concatBoth
-      $= C.map E.encodeUtf8 $= sinkHandle output
+      $= C.map encodeUtf8 $= sinkHandle output
 
 
 testConduit' :: IO ()
