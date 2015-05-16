f8d56aeb52fe409ffa75e3849ecef8e38f27215f
QuentinDuval <senmenty@gmail.com>
2015-05-16 19:15:41 +0200

    Sodium

--- src/SodiumTest.hs
+++ src/SodiumTest.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, MultiWayIf #-}
 module SodiumTest where
 
-
+import            Control.Applicative
 import            Control.Monad
+import            Control.Monad.IO.Class
+import qualified  Data.ByteString as B
+import            Data.Conduit as C
 import            Data.Text(Text)
 import qualified  Data.Text as T
+import            Data.Text.Encoding(decodeUtf8)
 import            FRP.Sodium
 
 
@@ -38,17 +42,26 @@ exampleWorkflow = do
             , eventS3 = filterE (\t -> T.length t <= 3) eventSink})
 
 
+userInput :: (MonadIO m) => Source m Text
+userInput = forever $
+   yield =<< decodeUtf8 <$> B.init <$> liftIO B.getLine
+
+
+userSink :: (MonadIO m) => Sources -> Sink Text m ()
+userSink input = awaitForever $ \t ->
+   liftIO $ sync $
+      if | "on" == t  -> setBool input True
+         | "off" == t -> setBool input False
+         | otherwise  -> setText input t
+
+
 test :: IO()
 test = do
    let listener s = putStrLn . (++) (s ++ ": ") . T.unpack
-   
    input <- sync $ do
       (sources, sinks) <- exampleWorkflow
       void $ listen (eventS1 sinks) (listener "l1")
       void $ listen (eventS2 sinks) (listener "l2")
       void $ listen (eventS3 sinks) (listener "l3")
       return sources
-      
-   mapM_ (sync . setText input) ["Trying", "out"]
-   sync  (setBool input False)
-   mapM_ (sync . setText input) ["Sodium", "!"]
+   runConduit $ userInput $$ userSink input
