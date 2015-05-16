d03b030d4e9a9addc31e48ec4cada58ea4a21aa9
QuentinDuval <senmenty@gmail.com>
2015-05-16 10:57:48 +0200

    Memoization

--- benchs/MemoizationBench.hs
+++ benchs/MemoizationBench.hs
@@ -10,9 +10,9 @@ memoizationBenchs =
    let n = 1000000
    in bgroup "memoization"
       [ bench "noMemoF"        $ nf noMemoF n
-      , bench "withMemoTree"   $ nf withMemoTree n
       , bench "withMemoList"   $ nf withMemoList n
-      , bench "withMemoMutMap" $ nf withMemoMutMap n
+      , bench "withMemoVect"   $ nf withMemoVect n
       , bench "withMemoStMap"  $ nf withMemoStMap n
       , bench "withMemoStHMap" $ nf withMemoStHMap n
-      , bench "withMemoVect"   $ nf withMemoVect n ]
+      , bench "withMemoMutMap" $ nf withMemoMutMap n
+      , bench "withMemoTree"   $ nf withMemoTree n ]
