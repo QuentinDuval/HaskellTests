38f4c00d47d4642b5e808d4f01cfc3410753e4bf
QuentinDuval <senmenty@gmail.com>
2015-06-28 15:08:58 +0200

    CompoundState

---
+++ src/MonadTry/CompoundState.hs
@@ -0,0 +1,22 @@
+module MonadTry.CompoundState where
+
+
+
+import Control.Monad.State
+
+
+
+{- |
+Given a getter (a -> b), and a setter (a -> b -> a)
+transform the current state from MonadState a to MonadState b.
+-}
+withSubState :: Monad m => (s1 -> s2) -> (s1 -> s2 -> s1) -> StateT s2 m a -> StateT s1 m a
+withSubState getter setter f = do
+   fullState <- get
+   let projState = getter fullState
+   (res, newState) <- lift $ runStateT f projState
+   put (setter fullState newState)
+   return res
+
+-- | CHECK - It might be the same as Control.Lens.Zoom
+
