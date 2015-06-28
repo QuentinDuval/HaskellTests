211ef9cdeaa9a0c05455d477a735f2ab39fa403b
QuentinDuval <senmenty@gmail.com>
2015-06-28 17:03:02 +0200

    Compound states tries

--- src/MonadTry/CompoundState.hs
+++ src/MonadTry/CompoundState.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE MultiWayIf, FlexibleContexts, OverloadedStrings, ScopedTypeVariables #-}
 module MonadTry.CompoundState where
 
-import Control.Lens.Zoom
-import Control.Monad.State
-
+import Control.Lens
+import Control.Monad
+--import Control.Monad.State
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State
+import Data.Conduit
+import Data.Conduit.List
+import Data.Text as T
 
 
 {- |
@@ -19,7 +25,66 @@ withSubState getter setter f = do
 
 
 -- | CHECK - It might be the same as Control.Lens.Zoom
+-- | zoom :: Monad m             => Lens' s t      -> StateT t m a -> StateT s m a
 
 
+-- | Test
+-- | Based on the fact that:
+-- MonadState s m => MonadState s (ConduitM i o m)
+-- MonadTrans (ConduitM i o)
+
+
+countShort :: (Monad m) => Conduit Text (StateT Int m) Text
+countShort =
+   awaitForever $ \t -> do
+      when (T.length t > 5) $
+         lift $ modify (+1)
+      yield t
+
+
+countShort' :: (Monad m) => Conduit Text (StateT ([Text], Int) m) Text
+countShort' =
+   awaitForever $ \t -> do
+      when (T.length t > 5) $
+         lift $ zoom _2 $ modify (+1)
+      yield t
+
+
+keepPalindroms :: (Monad m) => Conduit Text (StateT [Text] m) Text
+keepPalindroms =
+   awaitForever $ \t -> do
+      when (T.reverse t == t) $
+         lift $ modify (t:)
+      yield t
+
+
+keepPalindroms' :: (Monad m) => Conduit Text (StateT ([Text], Int) m) Text
+keepPalindroms' =
+   awaitForever $ \t -> do
+      when (T.reverse t == t) $
+         lift $ zoom _1 $ modify (t:)
+      yield t
+
+
+test :: IO ()
+test = do
+   print =<< (flip execStateT (1, 2) $ zoom _1 $ id .= 3)
+
+   let l = ["a", "ab", "abcdedcba", "zz"]
+
+   (ps, n) <-
+      runConduit $ 
+         flip execStateT ([], 0) $
+            sourceList l $$ countShort' =$ keepPalindroms' =$ sinkNull
+   
+   print ps
+   print n
+    
+   {-
+   flip runStateT ([] :: [Text], 0 :: Int) $
+      runConduit $
+         sourceList l $$ (zoom _2 countShort) =$ (zoom _1 keepPalindroms) =$ sinkNull
+   -}
+   return ()
 
 
