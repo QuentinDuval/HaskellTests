ae53593b3c159f67c532562325392aa0fdf475eb
QuentinDuval <senmenty@gmail.com>
2015-06-28 13:47:21 +0200

    Cont

--- src/MonadTry/ContMonad.hs
+++ src/MonadTry/ContMonad.hs
@@ -5,21 +5,22 @@ module MonadTry.ContMonad where
 import Control.Monad.Cont
 import Control.Monad.State
 import Data.Monoid
-import Data.Text
+import Data.Text as T
 
 
 
 data ParseEvent = OpenBlock | CloseBlock | Stop | Content Char
 
 parseEngine :: (Monad m) => Text -> ContT r m ParseEvent
-parseEngine txt = ContT $ \handler ->
-   case uncons txt of
-      Nothing     -> handler Stop
-      Just (h, t) -> do
-         if | h == '['  -> handler OpenBlock
-            | h == ']'  -> handler CloseBlock
-            | otherwise -> handler (Content h)
-         runContT (parseEngine t) handler
+parseEngine txt =
+   ContT $ \handler ->
+      case uncons txt of
+         Nothing     -> handler Stop
+         Just (h, t) -> do
+            if | h == '['  -> handler OpenBlock
+               | h == ']'  -> handler CloseBlock
+               | otherwise -> handler (Content h)
+            runContT (parseEngine t) handler
 
 
 
@@ -30,13 +31,35 @@ parseHandler (Content c) = modify (<> singleton c) >> get
 parseHandler Stop        = get
 
 
+indentHandler :: (MonadState Int m) => ParseEvent -> ContT r m Text
+indentHandler = handle
+   where
+      handle OpenBlock   = do
+         r <- withIndent "{"
+         modIndent 1
+         return r
+      
+      handle CloseBlock  = modIndent (-1) >> withIndent "}"
+      handle (Content c) = withIndent $ singleton c <> ";"
+      handle Stop = do
+         v <- get
+         return $ if v == 0
+            then "[Success]"
+            else "[Failure]"
+      
+      modIndent i = modify (+ i)
+      withIndent c = do
+         i <- get
+         return $ (T.replicate i "  ") <> c
+
+
 sinkHandler :: (MonadIO m, Show a) => a -> m ()
 sinkHandler = liftIO . print
 
 
 test :: IO ()
 test = do
-   flip runStateT "" $
-      runContT (parseEngine "[a[ab]c]" >>= parseHandler) sinkHandler
+   flip runStateT 0 $
+      runContT (parseEngine "[a[ab]c]" >>= indentHandler) sinkHandler
    return ()
 
