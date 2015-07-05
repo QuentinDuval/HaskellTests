66efc66ed0e32853e18e10d94d6cdce0361f9f76
QuentinDuval <senmenty@gmail.com>
2015-07-05 16:27:00 +0200

    More on pipes

--- src/Lib/PipeTry.hs
+++ src/Lib/PipeTry.hs
@@ -8,6 +8,10 @@ import qualified Pipes.Prelude as Pipes
 test :: IO ()
 test = do
 
+
+   -- TODO : work with
+   -- https://www.fpcomplete.com/school/to-infinity-and-beyond/pick-of-the-week/Pipes%20tutorial
+
    -- ^ Works for any Foldable
    runEffect $ for (each              [1 .. 4 :: Int]) (lift . putStr . show)
    runEffect $ for (each $ S.fromList [1 .. 4 :: Int]) (lift . putStr . show)
@@ -19,6 +23,8 @@ test = do
    -- ^ Using: (f ~> g) x = for (f x) g
    runEffect $ (each ~> lift . putStr . show) [1 .. 4 :: Int]
    
+   
+   runEffect $ each [1 .. 4 :: Int] >-> Pipes.map show >-> Pipes.stdoutLn
    
    -- ^ Equivalent formulations
    runEffect $ lift getLine  >~  Pipes.takeWhile (/= "exit") >-> Pipes.stdoutLn
