d62e54055ca934db09fa0a038de4d54738ef5c63
QuentinDuval <senmenty@gmail.com>
2015-07-14 11:57:16 +0200

    Refactoring

--- src/Performance/IOPartialSorting.hs
+++ src/Performance/IOPartialSorting.hs
@@ -7,12 +7,13 @@ import Control.Monad.Trans.Resource
 import Control.Monad.Trans.Class(lift)
 import Control.Monad.Trans.State(modify')
 
+-- import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Builder as BB
 
 import Data.Conduit as C
-import qualified Data.Conduit.Binary as B
-import qualified Data.Conduit.List as C
+import qualified Data.Conduit.Binary as CB
+import qualified Data.Conduit.List as CL
 import Data.Conduit.Lift
 
 import Data.Heap(MinPrioHeap)
@@ -42,6 +43,14 @@ testSlow = do
    let res = take 5000 $ sortBy (flip $ comparing snd) dataSet
    print res
 
+{-
+testByteString :: IO ()
+testByteString = do
+   inputs <- B.lines <$> B.readFile "IOPartialSorting.txt"
+   let dataSet = (\[x,y] -> (x, read y :: Int)) <$> (take 2 . words) <$> inputs
+   let res = take 5000 $ sortBy (flip $ comparing snd) dataSet
+   print res
+-}
 
 
 
@@ -50,10 +59,10 @@ testSlow = do
 testQuick :: IO ()
 testQuick = withFile "IOPartialSorting.txt" ReadMode $ \h -> do
       res <- runConduit $
-               B.sourceHandle h $$ B.lines
-               =$ C.map decodeUtf8
-               =$ C.map (take 2 . T.words)
-               =$ C.map (\[x,y] -> (x, read (T.unpack y) :: Int))
+               CB.sourceHandle h $$ CB.lines
+               =$ CL.map decodeUtf8
+               =$ CL.map (take 2 . T.words)
+               =$ CL.map (\[x,y] -> (x, read (T.unpack y) :: Int))
                =$ chunksOfConduit 1
                =$ computeRes 5000
       print res
@@ -90,10 +99,10 @@ chunksOfConduit nb = loop []
 
 fillInput :: IO ()
 fillInput =
-   do let filler = C.sourceList [1..1000000]
-                     $$ C.map (\c -> ("aaaa", c))
-                     =$ C.map (\(t, c) -> t <> " " <> BB.intDec c <> "\n")
-                     =$ C.map (BB.toLazyByteString)
-                     =$ C.map (BL.toStrict)
-                     =$ B.sinkFile "IOPartialSorting.txt" 
-      runResourceT $ filler
+   do let filler = CL.sourceList [1..1000000]
+                     $$ CL.map (\c -> ("aaaa", c))
+                     =$ CL.map (\(t, c) -> t <> " " <> BB.intDec c <> "\n")
+                     =$ CL.map BB.toLazyByteString
+                     =$ CL.map BL.toStrict
+                     =$ CB.sinkFile "IOPartialSorting.txt" 
+      runResourceT filler
