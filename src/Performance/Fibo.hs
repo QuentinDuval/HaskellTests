13014034cdaa0ad88c9fbb0ff240e7f147b580de
QuentinDuval <senmenty@gmail.com>
2015-07-19 11:46:21 +0200

    Refactoring

--- src/Performance/Fibo.hs
+++ src/Performance/Fibo.hs
@@ -36,14 +36,15 @@ fibFast = fibVal . fibPow
       -- Fast exponentiation
       fibPow n
          | n <= 0    = (1, 0, 0, 1) -- Identity matrix
-         | even n    = next
-         | otherwise = fibMult next fibMat
-         where next  = fibSq $! fibPow (div n 2)
+         | even n    = fibSq $! fibPow (div n 2)
+         | otherwise = fibMult fibMat $! fibPow (n - 1)
       
       fibSq m = fibMult m m
       fibMult (a , b , c , d ) (a', b', c', d')
          = ( a * a' + b * c', a * b' + b * d'
            , c * a' + d * c', c * b' + d * d')
+
+
 
 
 fibCont :: Int -> Integer
